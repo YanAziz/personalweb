59b0cf06165cb763bcddef311fa4be97e4c6e24f
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2024-04-01 20:43:20 +0700

    about-section

--- personal-website/src/app/components/Navbar.jsx
+++ personal-website/src/app/components/Navbar.jsx
@@ -22,8 +22,8 @@ const navLinks = [
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between mx-auto py-2 px-4">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex flex-wrap items-center justify-between mx-auto py-4 px-8">
         <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
           LOGO
         </Link>
