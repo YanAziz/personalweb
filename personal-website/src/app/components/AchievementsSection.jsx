ab0b23356e9a3d16a400278f890dec5eaadb94ce
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2024-04-02 21:26:09 +0700

    achievements-section

--- personal-website/src/app/components/AchievementsSection.jsx
+++ personal-website/src/app/components/AchievementsSection.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+
+const achievementsList = [
+  {
+    metric: "Projects",
+    value: "100+",
+  },
+  {
+    metric: "Github Stars",
+    value: "100+",
+  },
+  {
+    metric: "Users",
+    value: "100+",
+  },
+  {
+    metric: "Years",
+    value: "1+",
+  },
+];
 
 const AchievementsSection = () => {
   return (
-    <div>
-      
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
+        {achievementsList.map((achievement, index) => {
+          return (
+            <div key={index} className="flex flex-col justify-center items-center mx-4">
+              <h2 className="text-white font-bold text-4xl">{achievement.metric}</h2>
+              <p className="text-[#ADB7BE] text-base">{achievement.value}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AchievementsSection;
